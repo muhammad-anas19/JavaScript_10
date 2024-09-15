79b4899811c8385a000b200d41756eaa3d58e58d
Muhammad Anas <157979121+muhammad-anas19@users.noreply.github.com>
2024-09-15 12:13:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// MATH OBJECT
+// value of PI
+console.log(Math.PI);
+// Max number
+console.log(Math.max(38,11,10,33,4,29,76));
+// Min number
+console.log(Math.min(38,11,10,33,4,29,76));
+// Round off
+console.log(Math.round(1.4));
+// Floor
+console.log(Math.floor(1.7));
+// ceil
+console.log(Math.ceil(1.7));
+// Absolute
+console.log(Math.abs(-6));
+// square root
+console.log(Math.sqrt(9));
+
+
+// DATE OBJECT
+let currDate = new Date();
+console.log(currDate);
+let curr2 = new Date('June 23 2004 5:00');
+console.log(curr2);
+let curr3 = new Date(2004, 5, 23, 7);
+console.log(curr3);
